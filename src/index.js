3d56d61303f512c2ab2a89c7cc95cac24b27c93c
Riadh Gharbi <riadh.gharbi@yaiglobal.com>
2021-02-02 15:55:23 +0100

    tutorial videos 1 => 4

--- src/index.js
+++ src/index.js
@@ -4,14 +4,58 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { createStore} from "redux";
+const initialState = {
+    result: 1,
+    lastValues: [],
+    username:"Max"
+}
+
+const mathReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "ADD":
+            state = {
+                ...state,
+                result: state.result +action.payload,
+                lastValues:[...state.lastValues,action.payload]
+            }
+            break;
+        case "SUBTRACT":
+            state = {
+                ...state,
+                result: state.result -action.payload,
+                lastValues:[...state.lastValues,action.payload]
+            }
+            break;
+    }
+    return state;
+}
+
+const store = createStore(mathReducer);
+
+store.subscribe(()=>{
+    console.log("store updated !!", store.getState())
+})
+
+store.dispatch({
+    type:"ADD",
+    payload:100
+})
+
+store.dispatch({
+    type:"SUBTRACT",
+    payload:80
+})
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
